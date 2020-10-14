ff2337411da672a047e6eb32fa4852026baa11b2
GolfWang777 <52179832+GolfWang777@users.noreply.github.com>
2020-10-14 14:03:06 +0500

    Задание номер 1
    Номер 1 задание

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырехзначное число");
+            int number = int.Parse(Console.ReadLine());
+
+            int a = number % 10;
+            int b = (number % 100 - number % 10) / 10;
+            int c = (number % 1000 - number % 100) / 100;
+            int d = number / 1000;
+
+
+            Console.WriteLine("Число единиц: " + a);
+            Console.WriteLine("Число тысяч: " + d);
+            Console.Write("произведение цифр" + d * b * c * a);
+
+            Console.ReadKey();
+
+        }
+    }
+}
