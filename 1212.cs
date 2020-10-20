7a2904b7af883183c7c9ef8d5a8daec44e1fca32
GolfWang777 <52179832+GolfWang777@users.noreply.github.com>
2020-10-20 22:34:04 +0500

    Create 1212.cs

---
+++ 1212.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3312
+{
+    class Program
+    {
+        static double SinFunction()
+        {
+            double y;
+            y = Math.Sin(4) + 3;
+for (int i = 2; i >= 0; i--)
+            {
+                y = Math.Sin(y) + i;
+            }
+            return y;
+        }
+    }
+    }
+
