85cf1acbe6d1728128fb19e2db635f9f1153a816
GolfWang777 <52179832+GolfWang777@users.noreply.github.com>
2020-12-28 01:06:39 +0500

    Задание 5-2

---
+++ DZ2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string original = Console.ReadLine();
+            int length = original.Length;
+            for (int i = 0; i < length; i = i + 1)
+            {
+                string temp = (original[i]).ToString();
+                Console.Write(Function(temp));
+                if (i == length - 1)
+                    Console.WriteLine("");
+                if s[i] = ("а") then s [i] := ("a");
+            if s[i] = ("б") then s[i] := ("b");
+            if s[i] = ("в") then s[i] := ("v");
+            if s[i] = ("г") then s[i] := ("g");
+            if s[i] = ("д") then s[i] := ("d");
+            if s[i] = ("е") then s[i] := ("e");
+            if s[i] = ("ё") then s[i] := ("e");
+            if s[i] = ("ж") then s[i] := ("zh");
+            if s[i] = ("з") then s[i] := ("z");
+            if s[i] = ("и") then s[i] := ("i");
+            if s[i] = ("й") then s[i] := ("I");
+            if s[i] = ("к") then s[i] := ("k");
+            if s[i] = ("л") then s[i] := ("l");
+            if s[i] = ("м") then s[i] := ("m");
+            if s[i] = ("н") then s[i] := ("n");
+            if s[i] = ("о") then s[i] := ("o");
+            if s[i] = ("п") then s[i] := ("p");
+            if s[i] = ("р") then s[i] := ("r");
+            if s[i] = ("с") then s[i] := ("s");
+            if s[i] = ("т") then s[i] := ("t");
+            if s[i] = ("у") then s[i] := ("u");
+            if s[i] = ("ф") then s[i] := ("f");
+            if s[i] = ("х") then s[i] := ("kh");
+            if s[i] = ("ц") then s[i] := ("ts");
+            if s[i] = ("ч") then s[i] := ("ch");
+            if s[i] = ("ш") then s[i] := ("sh");
+            if s[i] = ("щ") then s[i] := ("shch");
+            if s[i] = ("ъ") then s[i] := ("ie");
+            if s[i] = ("ы") then s[i] := ("y");
+            if s[i] = ("э") then s[i] := ("e");
+            if s[i] = ("ю") then s[i] := ("iu");
+            if s[i] = ("я") then s[i] := ("ya");
+
+
+        }
+    }
+
+
+
+
+}
