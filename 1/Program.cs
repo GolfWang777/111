548abfa32976c240c091b71eee07eca7d325cd53
GolfWang777 <52179832+GolfWang777@users.noreply.github.com>
2020-12-28 00:17:14 +0500

    Задание 8 номера 1,2,4,5

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+
+                double x = 0;
+                double pi = 3.14159265358979323846264;
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < 2 * pi;)
+                {
+
+                    x = Math.Sin(i);
+                    Console.WriteLine(x);
+                    i = i + (pi / n);
+                }
+
+            }
+        }
+
---
+++ 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 0;
+            int c = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                b = numbers[i] + numbers[i + 1];
+                if (c < b)
+                {
+                    c = b;
+                }
+            }
+            Console.WriteLine(c);
+            //Your code goes here
+
+        }
+    }
+}
