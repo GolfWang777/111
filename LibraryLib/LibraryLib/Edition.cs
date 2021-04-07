fc850ec6a2a3cd54c07e1ed93f7a8a6d1541e46e
GolfWang777 <52179832+GolfWang777@users.noreply.github.com>
2021-04-07 23:02:18 +0500

    Задание 11 выполнено

---
+++ LibraryLib/LibraryLib/Edition.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LibraryLib
+{
+    public class Edition
+    {
+        public string Title;
+        public string ListOfAuthors;
+        public int YearPublished;
+        public string Publisher;
+        public readonly long UniqueID;
+        public Location WhereBook;
+
+        public Edition(string title, string listOfAuthors, int yearPublished, string publisher, long uniqueID, Location whereBook)
+        {
+            Title = title;
+            ListOfAuthors = listOfAuthors;
+            YearPublished = yearPublished;
+            Publisher = publisher;
+            UniqueID = uniqueID;
+            WhereBook = whereBook;
+        }
+        public override string ToString()
+        {
+            return $"Книга: {Title}. Автор(ы): {ListOfAuthors}.";
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine(this);
+
+            var whereBook = "";
+            switch (WhereBook)
+            {
+                case Location.Home:
+                    whereBook = "выдана на дом";
+                    break;
+                case Location.ReadingRoom:
+                    whereBook = "выдана в читательный зал";
+                    break;
+                case Location.Storage:
+                    whereBook = "в хранилище";
+                    break;
+            }
+           
+            Console.WriteLine($"Год издания: {YearPublished}. Издатель: {Publisher}. Инвентарный номер: {UniqueID}. Статус: {whereBook}.");
+        }
+    }
+}
