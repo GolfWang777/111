c6f4a9d7dcd06b4bc5ebbbb88e79c9d0828f4168
GolfWang777 <52179832+GolfWang777@users.noreply.github.com>
2021-12-28 13:54:58 +0500

    Зачет сделан

---
+++ Geometry3D/Geometry3D/Bodies/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry3D
+{
+    public class Ball : Body
+    {
+        public double Radius { get; }
+
+        public Ball(Vector3 position, double radius) : base(position)
+        {
+            Radius = radius;
+        }
+		
+		public override bool ContainsPoint(Vector3 point) {
+			var vector = point - Position;
+			var length2 = vector.GetLength2();
+			return length2 <= ball.Radius * ball.Radius;
+		}
+		
+		public override RectangularCuboid GetBoundingBox()
+        {
+			var length = this.Radius * 2;
+            return new RectangularCuboid(this.Position, length, length, length);
+        }
+    }
+
+}
