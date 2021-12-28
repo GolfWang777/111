03f05ca39df27087d7fddab162a95db355680de5
GolfWang777 <52179832+GolfWang777@users.noreply.github.com>
2021-12-28 17:15:20 +0500

    3

--- Geometry3D/Geometry3D/Bodies/Cone.cs
+++ Geometry3D/Geometry3D/Bodies/Cone.cs
@@ -8,31 +8,19 @@ namespace Geometry3D
 {
     public class Cone : Body
     {
-        public double SizeZ { get; }
-
-        public double Radius { get; }
-
-        public Cone(Vector3 position, double sizeZ, double radius) : base(position)
+        public override bool ContainsPoint(Vector3 point)
         {
-            SizeZ = sizeZ;
-            Radius = radius;
+            var x_0 = point.X - this.Position.X;
+            var y_0 = point.Y - this.Position.Y;
+            var z_0 = point.Z - this.Position.Z;
+            var r = Math.Sqrt(x_0 * x_0 + y_0 * y_0);
+
+            var k = -z_0 / this.Radius;
+
+            return k * r + z_0 <= point.Z && this.Position.X - this.Radius <= point.X <= this.Position.X + this.Radius && this.Position.Y - this.Radius <= point.Y <= this.Position.Y + this.Radius;
         }
-		
-		public override bool ContainsPoint(Vector3 point) {
-			var x_0 = point.X - this.Position.X;
-			var y_0 = point.Y - this.Position.Y;
-			var z_0 = point.Z - this.Position.Z;
-			var r = Math.Sqrt(x_0 * x_0 + y_0 * y_0);
-			
-			var k = - z_0 / this.Radius;
-			
-			return k * r + z_0 <= point.Z && this.Position.X - this.Radius <= point.X <= this.Position.X + this.Radius && this.Position.Y - this.Radius <= point.Y <= this.Position.Y + this.Radius;
-		}
-		
-		public override RectangularCuboid GetBoundingBox()
-        {
-            return new RectangularCuboid(this.Position, this.Radius * 2, this.Radius * 2, this.SizeZ);
-        }
+
     }
+}
 
 }
